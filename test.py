765a6cebb5930683a0884d6b3768cfcef1b8aa07
Sun <js_syw@qq.com>
2022-08-02 10:49:02 +0800

    20220802-...

--- test.py
+++ test.py
@@ -4,23 +4,28 @@ import pandas as pd
 from lxml import etree
 import re
 
-a=[1,2,3,4,5,6]
-print(a[:-1])
-# csv_file_path = './urls_unique.csv'
-# url_df = pd.read_csv(csv_file_path, encoding='utf-8', engine='python')
-# url_df = url_df.reindex(columns=url_df.columns.tolist() + ["redirect_url"])
-# url_df.to_csv('./redirect_urls.csv', index=False)
+list = []
+csv_file_path = './notsure.csv'
+url_df = pd.read_csv(csv_file_path, encoding='utf-8', engine='python')
+# url_df = url_df.drop_duplicates()
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+}
+proxies = {
+    'http': 'http://127.0.0.1:1080',
+    'https': 'http://127.0.0.1:1080'
+}
+urls = url_df.iloc[:, 0]
+for url in urls:
+    url = "http://" + url
+    print(url)
+    res = requests.get(url, headers=headers, timeout=8, proxies=proxies)
+    print("{0}: {1}".format(url, res.status_code))
+    if res.status_code == "200":
+        list.append(url)
+df = pd.DataFrame(list, columns=['url'])
+df.to_csv('./notsure_unique_visitable.csv', index=False)
 
-# headers = {
-#     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
-# }
-# proxies = {
-#     'http': 'http://127.0.0.1:1080',
-#     'https': 'http://127.0.0.1:1080'
-# }
-# url = "https://t.co/WsnLeguMXX"
-# res = requests.get(url, headers=headers, timeout=8, proxies=proxies)
-# print(res.status_code)
 # print(res.text)
 # print(res.url)
 # html = etree.HTML(res.text)
